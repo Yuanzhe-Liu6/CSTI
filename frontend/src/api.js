67fd1d1838eeaa5e284f802a24737f0df270aa53
Yuanzhe <408788385@qq.com>
2026-04-10 20:57:01 -0700

    deploy frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const client = axios.create({ baseURL: '/api' });
+const baseURL = import.meta.env.VITE_API_BASE_URL
+  ? `${import.meta.env.VITE_API_BASE_URL}/api`
+  : '/api';
+
+const client = axios.create({ baseURL });
 
 export const fetchQuiz = () => client.get('/quiz').then((r) => r.data);
-
 export const submitAnswers = (answers) =>
   client.post('/submit', { answers }).then((r) => r.data);
-
-export const fetchResult = (id) => client.get(`/result/${id}`).then((r) => r.data);
-
+export const fetchResult = (id) =>
+  client.get(`/result/${id}`).then((r) => r.data);
 export const fetchStats = () => client.get('/stats').then((r) => r.data);
